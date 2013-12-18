d2b8112a09f40d3d26856a4cca0efe70ae5c43e8
m1ke <git@m1ke.me>
2013-12-18 11:48:02 +0000

    Change to fb app

--- js/home.js
+++ js/home.js
@@ -78,7 +78,7 @@ fbQuickApp.prototype.postToFeed=function(post,callback){
 };
 fbQuickApp.prototype.userLoggedIn=function(){
 	$(this.loginEl).parent().html('Logged in with Facebook').addClass('logged-in');
-	$('.fb-app').fadeIn();
+	$('.fb-app').fadeIn().trigger('fb-login');
 };
 fbQuickApp.prototype.userNotLoggedIn=function($link){
 	$link.text('Please try again');
@@ -113,6 +113,26 @@ fbFunctionQ.push(function(){
 	}
 });
 
-$(function(){
+var christmasFace={
+	getPicture:function($app){
+		if (!$app){
+			$app=$('.fb-app');
+		}
+		fbChristmas.api('/me/picture',{redirect:false,type:'large'},function($app){
+			return function(response){
+				$(this).find('img.fb-pic').remove();
+				$('<img src="'+response.url+'">').addClass('fb-pic').prependTo($(this));
+			};
+		}($app));
+	}
+};
 
+$(function(){
+	$('.fb-app').bind('fb-login',function(){
+		christmasFace.getPicture($(this));
+	});
+	$('a.get-picture').click(function(e){
+		e.preventDefault();
+		christmasFace.getPicture();
+	});
 });
--- jspp/home.js
+++ jspp/home.js
@@ -13,6 +13,26 @@ fbFunctionQ.push(function(){
 	}
 });
 
-$(function(){
+var christmasFace={
+	getPicture:function($app){
+		if (!$app){
+			$app=$('.fb-app');
+		}
+		fbChristmas.api('/me/picture',{redirect:false,type:'large'},function($app){
+			return function(response){
+				$(this).find('img.fb-pic').remove();
+				$('<img src="'+response.url+'">').addClass('fb-pic').prependTo($(this));
+			};
+		}($app));
+	}
+};
 
+$(function(){
+	$('.fb-app').bind('fb-login',function(){
+		christmasFace.getPicture($(this));
+	});
+	$('a.get-picture').click(function(e){
+		e.preventDefault();
+		christmasFace.getPicture();
+	});
 });
