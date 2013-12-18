47c1791fa53d04c95196dcc7bd2639c6637c3f4e
m1ke <git@m1ke.me>
2013-12-18 12:29:10 +0000

    Test detect

--- jspp/home.js
+++ jspp/home.js
@@ -15,6 +15,8 @@ fbFunctionQ.push(function(){
 });
 
 var christmasFace={
+	elFbImg:'.fb-pic'
+	elDetectImg:'.img',
 	domSetup:function(){
 		var self=this;
 		$('.fb-app').bind('fb-login',function(){
@@ -26,7 +28,8 @@ var christmasFace={
 		});
 		$('a.get-face').click(function(e){
 			e.preventDefault();
-			var coords=$('img.test').faceDetection({
+			var $img=$(self.elDetectImg);
+			var coords=$img.find('img').faceDetection({
 				error:function(img,code,message){
 					console.log(img);
 					console.log(code);
@@ -34,16 +37,30 @@ var christmasFace={
 				}
 			});
 			console.log(coords);
+			for (var i = 0; i < coords.length; i++) {
+				$('<div>', {
+					'class':'face',
+					'css': {
+						'position':	'absolute',
+						'left':		coords[i].positionX +5+'px',
+						'top':		coords[i].positionY +5+'px',
+						'width': 	coords[i].width		+'px',
+						'height': 	coords[i].height	+'px'
+					}
+				})
+				.appendTo($img);
+			}
 		});
 	},
 	getPicture:function($app){
+		var self=this;
 		if (!$app){
 			$app=$('.fb-app');
 		}
 		FB.api('/me/picture',{redirect:false,type:'large'},function($app){
 			return function(response){
-				$app.find('img.fb-pic').remove();
-				$('<img src="'+response.data.url+'">').addClass('fb-pic').prependTo($app);
+				$app.find('div.fb-pic').remove();
+				$('<img src="'+response.data.url+'">').prependTo($app).wrap('<div class="'+self.elFbImg+'">');
 				$('a.get-face').fadeIn();
 			};
 		}($app));
