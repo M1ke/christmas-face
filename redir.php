dadedc991186d1a66f28dc541bea764b25ad7fa9
m1ke <git@m1ke.me>
2013-12-18 13:03:32 +0000

    save images

--- redir.php
+++ redir.php
@@ -1,7 +1,7 @@
 <?php
 $img=file_get_contents($_GET['file']);
 $file=$_GET['file'];
-$file=substr($file,strrpos('/',$file)+1);
+$file=substr($file,strrpos($file,'/')+1);
 $fh=fopen('images/'.$file,'w');
 if (!empty($fh)){
 	fwrite($fh,$img);
